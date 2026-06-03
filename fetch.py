06579565fa1e981a6aa9a1ed3a5c3abe9d8dc25b
minghuiwang603 <minghuiwang603@gmail.com>
2026-06-03 11:48:03 +0800

    print status and headers

--- fetch.py
+++ fetch.py
@@ -1,4 +1,7 @@
 import requests 
-resp = requests.get("https://example.com")
-print(resp.status_code)
-print(len(resp.text))
+resp = requests.get("https://www.ruanyifeng.com/blog/atom.xml")
+print(f"状态码: {resp.status_code}")
+print(f"Content-Type: {resp.headers['Content-Type']}")
+print(f"Server: {resp.headers['Server']}")
+print(f"Date: {resp.headers['Date']}")
+print(resp.text[:300])
