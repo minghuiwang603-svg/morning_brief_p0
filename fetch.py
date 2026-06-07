54370642b80f749c5a87bd665c1ce7039b7f42f2
minghuiwang603 <minghuiwang603@gmail.com>
2026-06-07 22:39:53 +0800

    add feedparser

--- fetch.py
+++ fetch.py
@@ -1,7 +1,12 @@
 import requests 
+import feedparser
 resp = requests.get("https://www.ruanyifeng.com/blog/atom.xml")
+d = feedparser.parse(resp.text)
+articles = []
 print(f"状态码: {resp.status_code}")
 print(f"Content-Type: {resp.headers['Content-Type']}")
 print(f"Server: {resp.headers['Server']}")
 print(f"Date: {resp.headers['Date']}")
-print(resp.text[:300])
+for entry in d.entries:
+    articles.append({"title" :entry.title,"link":entry.link})
+print(articles)
