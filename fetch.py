749772067b1d2f35fd32d36a470a92e18adcd466
minghuiwang603 <minghuiwang603@gmail.com>
2026-06-08 15:11:09 +0800

    break down fetch.py into fetch/parse/format

--- fetch.py
+++ fetch.py
@@ -1,12 +1,24 @@
 import requests 
 import feedparser
-resp = requests.get("https://www.ruanyifeng.com/blog/atom.xml")
-d = feedparser.parse(resp.text)
-articles = []
-print(f"状态码: {resp.status_code}")
-print(f"Content-Type: {resp.headers['Content-Type']}")
-print(f"Server: {resp.headers['Server']}")
-print(f"Date: {resp.headers['Date']}")
-for entry in d.entries:
-    articles.append({"title" :entry.title,"link":entry.link})
-print(articles)
+def fetch(url: str) -> str: 
+    resp = requests.get(url)
+    return resp.text
+
+
+def parse(text: str) -> list:
+    articles = []
+    d = feedparser.parse(text)
+    for entry in d.entries:
+        articles.append({"title" :entry.title,"link":entry.link})
+    return articles
+
+
+def format_brief(articles: list) -> str:
+    result = ""
+    for article in articles:
+        result += article["title"]
+        result += "\n"
+        result += article["link"]
+        result += "\n\n"
+    return result
+print(format_brief(parse(fetch("https://www.ruanyifeng.com/blog/atom.xml"))))
